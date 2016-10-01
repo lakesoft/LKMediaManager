f7399a299fdde4944ed12def8fa3ea01ef576bbc
Hiroshi Hashiguchi <xcatsan@mac.com>
2016-10-01 16:14:07 +0900

    Rmoved bridging-header

--- Example/Pods/Target Support Files/LKMediaManager/LKMediaManager-umbrella.h
+++ Example/Pods/Target Support Files/LKMediaManager/LKMediaManager-umbrella.h
@@ -1,7 +1,5 @@
 #import <UIKit/UIKit.h>
 
-#import "LKMediaManager-Bridging-Header.h"
-
 FOUNDATION_EXPORT double LKMediaManagerVersionNumber;
 FOUNDATION_EXPORT const unsigned char LKMediaManagerVersionString[];
 
